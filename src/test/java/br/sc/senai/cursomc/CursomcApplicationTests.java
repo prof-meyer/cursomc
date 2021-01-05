75b85f661c581cbd9e0f6cc49e19f6507597606f
Prof Meyer <rafael.meyer@edu.sc.senai.br>
2021-01-05 11:25:51 -0300

    projeto criado

---
+++ src/test/java/br/sc/senai/cursomc/CursomcApplicationTests.java
@@ -0,0 +1,13 @@
+package br.sc.senai.cursomc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursomcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
